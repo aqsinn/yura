e7d267525be7dbcfe27f29e046700854e9a6ea00
alakbarlii <alakbarlii002@gmail.com>
2026-05-13 02:38:39 +0400

    saving work

--- app/components/profile/PricingBanner.tsx
+++ app/components/profile/PricingBanner.tsx
@@ -69,7 +69,7 @@ const tiers = [
       'Priority support',
       'Early access to features',
     ],
-    buttonText: 'AlphaMaxed',
+    buttonText: 'Get AlphaMaxed',
     mostPopular: true,
     color: 'bg-indigo-500/10 text-indigo-500 border-indigo-500/20',
     badge: 'Premium',
